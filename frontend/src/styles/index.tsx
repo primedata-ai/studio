59b7a7b9a4d2678bcba63b2ab022ec9f6fa84587
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2023-08-08 15:47:52 +0700

    [feat] - update logo and header style

--- frontend/src/styles/index.tsx
+++ frontend/src/styles/index.tsx
@@ -1,2 +1,3 @@
 import './antd.less' /* Imports Ant Design's components */
 import './global.scss' /* Contains PostHog's main styling configurations */
+import "./prime.style.scss" /* Contains PrimeData VN`s main styling configurations */
